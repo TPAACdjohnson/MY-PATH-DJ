eddcb2ba3cb25136921acb2c631727fc085c5e2a
TPAACdjohnson <162364454+TPAACdjohnson@users.noreply.github.com>
2025-07-30 12:44:37 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,55 @@
-const CACHE_NAME = "mypath-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/story_hub.js",
-  "/chapter_one_game.js",
-  "/chapter_two_game.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+const CACHE_NAME = 'my-path-v2'; // Change this on every new deployment!
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/story_hub.js',
+  '/chapter_one_game.js',
+  '/chapter_two_game.js',
+  '/chapter_three_game.js',
+  '/manifest.json'
 ];
 
-// Install Service Worker
-self.addEventListener("install", (event) => {
-  console.log("[ServiceWorker] Install");
+// Install Event
+self.addEventListener('install', (event) => {
+  console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("[ServiceWorker] Pre-caching offline page");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[ServiceWorker] Caching all files');
+        return cache.addAll(urlsToCache);
+      })
   );
-  self.skipWaiting();
 });
 
-// Activate Service Worker
-self.addEventListener("activate", (event) => {
-  console.log("[ServiceWorker] Activate");
+// Activate Event: Clear old caches
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
   event.waitUntil(
     caches.keys().then((keyList) =>
       Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache", key);
+            console.log('[ServiceWorker] Removing old cache:', key);
             return caches.delete(key);
           }
         })
       )
     )
   );
-  self.clients.claim();
+  return self.clients.claim();
 });
 
-// Fetch logic
-self.addEventListener("fetch", (event) => {
-  console.log("[ServiceWorker] Fetch", event.request.url);
+// Fetch Event
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Else fetch from network
+        return fetch(event.request).then((res) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, res.clone());
