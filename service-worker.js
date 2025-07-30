c3a51eb77fd80eb9be3031d257a0fd5023b5c344
TPAACdjohnson <162364454+TPAACdjohnson@users.noreply.github.com>
2025-07-30 10:27:15 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,51 @@
+const CACHE_NAME = "mypath-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/story_hub.js",
+  "/chapter_one_game.js",
+  "/chapter_two_game.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('legacy-store').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/game.js',
-                '/manifest.json'
-            ]);
-        })
-    );
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  console.log("[ServiceWorker] Install");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  console.log("[ServiceWorker] Activate");
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch logic
+self.addEventListener("fetch", (event) => {
+  console.log("[ServiceWorker] Fetch", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
